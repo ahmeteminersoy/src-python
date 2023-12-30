2d6b4257ab32db61e3b25416fd70d4473527332b
metamfetemin <ahmet.eminersoy@icloud.com>
2023-12-30 18:51:13 +0300

    update

--- examples/examples.py
+++ examples/examples.py
@@ -1,9 +1,2 @@
-a = [1, 2, 3, 4, 5]
-s = ['ali', 'veli', 'selami', 'ayşe', 'fatma', 'cumhur']
-w = [72.4, 69.5, 84.2, 51.6, 56.2]
-
-def foo(*x):
-    return x
-
-for x in map(foo, a, s, w):
-    print(x)
+names = ['ali', 'veli', 'selami', 'ayşe', 'fatma']
+print(*[len(name) for name in names])
