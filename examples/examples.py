3dfd694e46134c082f1d26253cd42e81540da2ce
metamfetemin <ahmet.eminersoy@icloud.com>
2023-12-30 23:34:13 +0300

    update

--- examples/examples.py
+++ examples/examples.py
@@ -1,2 +1,19 @@
-names = ['ali', 'veli', 'selami', 'ayşe', 'fatma']
-print(*[len(name) for name in names])
+import time
+
+start = time.time()
+
+a = []
+
+for i in range(10000000):
+    a.append(str(i))
+    
+stop = time.time()
+
+print(stop - start)             
+    
+start = time.time()
+
+a = [str(i) for i in range(10000000)]
+stop = time.time()
+
+print(stop - start)            
